0bd214b751b613167fc1ab6c53d54963d83b65fd
Adrian Suarez <asuarez9382@gmail.com>
2024-06-03 08:34:47 -0700

    adds styling to dog form page

--- client/src/components/DogForm.js
+++ client/src/components/DogForm.js
@@ -6,29 +6,30 @@ function DogForm(){
         <div className="dog-form">
             <h1>Dog Form</h1>
             <form>
-                <div className="form-name">
-                    <label>Dog Name: </label>
+                <div className="form-group">
+                    <label>Dog Name:</label>
                     <input type='text' name='name' />
                 </div>
-                <div className="form-breed">
-                    <label>Dog Breed: </label>
+                <div className="form-group">
+                    <label>Dog Breed:</label>
                     <input type='text' name='breed' />
                 </div>
-                <div className="form-price">
-                    <label>Price: </label>
-                    <input type='integer' name='price' />
+                <div className="form-group">
+                    <label>Price:</label>
+                    <input type='number' name='price' />
                 </div>
-                <div className="form-age">
-                    <label>Age: </label>
-                    <input type='integer' name='age' />
+                <div className="form-group">
+                    <label>Age:</label>
+                    <input type='number' name='age' />
                 </div>
-                <div className="form-description">
-                    <label>Description: </label>
+                <div className="form-group">
+                    <label>Description:</label>
                     <input type='text' name='description' />
                 </div>
                 <button type="submit">Submit</button>
             </form>
         </div>
+
     );
 }
 
