982527f9713473282f3c93311b0b7843a8a9d682
Adrian Suarez <asuarez9382@gmail.com>
2024-06-06 14:48:38 -0700

    toggles nav bar to logout and user page instead of login and signup

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,48 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { DogContext } from "./AppContext";
 
 function NavBar() {
+
+    const { userData, setUserData } = useContext(DogContext);
+    
+
+
     return (
-        <nav>
+        <nav className="navbar">
             <div className="nav-left">
-                <Link
-                    to="/"
-                    className="nav-link"
-                >
-                    AdoptADog
-                </Link>
-                <Link
-                    to="/dogs"
-                    className="nav-link"
-                >
-                    Available Dogs
-                </Link>
-                <Link
-                    to="/listdog"
-                    className="nav-link"
-                >
-                    List a Dog for Adoption
-                </Link>
-                <Link
-                    to="/about"
-                    className="nav-link"
-                >
-                    About
-                </Link>
+                <Link to="/" className="nav-link">AdoptADog</Link>
+                <Link to="/dogs" className="nav-link">Available Dogs</Link>
+                <Link to="/listdog" className="nav-link">List a Dog for Adoption</Link>
+                <Link to="/about" className="nav-link">About</Link>
             </div>
             <div className="nav-right">
-                <Link
-                    to="/login"
-                    className="nav-link"
-                >
-                    Login
-                </Link>
-                <Link
-                    to="/signup"
-                    className="nav-link"
-                >
-                    Signup
-                </Link>
+                {userData ? (
+                    <>
+                        <Link to="/logout" className="nav-link">Logout</Link>
+                        <Link to={`/users/${userData.id}`} className="nav-link">{userData.username}</Link>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/login" className="nav-link">Login</Link>
+                        <Link to="/signup" className="nav-link">Signup</Link>
+                    </>
+                )}
             </div>
         </nav>
     );
