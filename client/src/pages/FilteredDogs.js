1e829e2dbfde1ac94737d57093e45b4bb1e50bf8
Adrian Suarez <asuarez9382@gmail.com>
2024-06-07 15:22:59 -0700

    makes filtered dogs page

--- client/src/pages/FilteredDogs.js
+++ client/src/pages/FilteredDogs.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { DogContext } from "../components/AppContext";
+import DogCard from "../components/DogCard";
 
 function FilteredDogs() {
 
+    const { filteredList, setFilteredList } = useContext(DogContext);
+
     return(
-        <h1>Filtered Dogs</h1>
+        <div className="filtered-list-container">
+            <h1 className="dog-list-title">Available {filteredList[0]['breed']}'s</h1>
+            <div className="filtered-list">
+                {filteredList.map(dog => (
+                    <DogCard
+                        key={dog.id}
+                        id={dog.id}
+                        name={dog.name}
+                        breed={dog.breed}
+                        image={dog.image}
+                        isAdopted={dog.isAdopted}
+                        age={dog.age}
+                        description={dog.description} 
+                        price={dog.price}
+                    />
+                ))}
+            </div>
+        </div>
     );
 }
 
