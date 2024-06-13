3c169c95c3e79822876aaa9dd49e433a8f7f1d21
Adrian Suarez <asuarez9382@gmail.com>
2024-06-13 13:15:07 -0700

    styles my favorited dogs page

--- client/src/pages/MyFavorites.js
+++ client/src/pages/MyFavorites.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { DogContext } from "../components/AppContext";
 
 
 function MyFavorites(){
 
-    ```
-    fetch("/favorites")
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok')
-            }
-            return response.json();
-          })
-          .then(data => console.log(data))
-          .catch(error => console.error('Error fetching favorites:', error));
-      }
-    ```
+  const { favoriteList } = useContext(DogContext);
+   
+  console.log(favoriteList)
 
     return(
-        <div>
-            <h1>My Favorites</h1>
+      <div className="user-page-container">
+        <div className="user-dog-container">
+            <h2 className="user-dog-title">My Favorited Dogs:</h2>
+            {favoriteList.map(favorite=>(
+              <div className="user-dog-details" key={favorite.dog.id}>
+                <h2>{favorite.dog.name}</h2>
+                <Link to={`/dogs/${favorite.dog.id}`}>
+                  <img src={favorite.dog.image} alt={favorite.dog.breed} className="user-dog-image" />
+                </Link>
+              </div>
+              )
+            )}
         </div>
+      </div>
     );
 }
 
