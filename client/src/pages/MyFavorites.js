511716c291cf25ee89fafcf233cd296345bff7f4
Adrian Suarez <asuarez9382@gmail.com>
2024-06-14 08:19:03 -0700

    implements delete favorite feature in front end

--- client/src/pages/MyFavorites.js
+++ client/src/pages/MyFavorites.js
@@ -5,9 +5,33 @@ import { DogContext } from "../components/AppContext";
 
 function MyFavorites(){
 
-  const { favoriteList } = useContext(DogContext);
+  const { favoriteList, setFavoriteList } = useContext(DogContext);
    
   console.log(favoriteList)
+
+
+  function handleClick(id){
+    fetch(`/favorites/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+    })
+    .then(data => {
+      // Update favoriteList to remove the deleted favorite
+      setFavoriteList(prevList => prevList.filter(fav => fav.id !== id));
+    })
+    .catch(error => {
+      console.error('Error removing favorite:', error);
+    });
+  }
+  //Next step add trash can icon to make delete request to api
+  //Add button to add a note about favorited dog
 
     return(
       <div className="user-page-container">
@@ -15,10 +39,12 @@ function MyFavorites(){
             <h2 className="user-dog-title">My Favorited Dogs:</h2>
             {favoriteList.map(favorite=>(
               <div className="user-dog-details" key={favorite.dog.id}>
+                
                 <h2>{favorite.dog.name}</h2>
                 <Link to={`/dogs/${favorite.dog.id}`}>
                   <img src={favorite.dog.image} alt={favorite.dog.breed} className="user-dog-image" />
                 </Link>
+                <button onClick={() => handleClick(favorite.id)}>x</button>
               </div>
               )
             )}
